a7ade2e59859d92f8e96f4821c7e6d21eed3d2e4
mattbibby95 <Mattbibby95@gmail.com>
2023-07-16 02:12:19 +0100

    changed bg colour

--- src/components/teamInfo.vue
+++ src/components/teamInfo.vue
@@ -20,7 +20,7 @@ const props = defineProps(['teamInfo', 'allHeroStats', 'teamName', 'teamWinrates
     flex-direction: column;
     align-items: center;
     box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
-    background-color: #F3F5F1;
+    background-color: #f3f5f1;
     margin: 20px;
 }
 .WinrateCard{
