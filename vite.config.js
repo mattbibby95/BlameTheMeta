95ef009fc2d1c7b15de735cc5427c7419b309f4e
mattbibby95 <Mattbibby95@gmail.com>
2023-07-16 22:52:08 +0100

    trying with empty base vite config

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/BlameTheMeta/',
   plugins: [
     vue(),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base: './',
 })
